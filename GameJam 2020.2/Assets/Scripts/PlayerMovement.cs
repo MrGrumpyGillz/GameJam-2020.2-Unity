cad58cfe11a96c195a0e75676ab67c72434caa12
Tyler <Timanian@mac.com>
2020-08-03 15:20:23 -0400

    Auto Ground Movement
    plus some stages to play with.

---
+++ GameJam 2020.2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    float horizMove = 0f;
+    public float moveSpeed = 50f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizMove * Time.fixedDeltaTime, false, false);
+        jump = false;
+
+    }
+}
